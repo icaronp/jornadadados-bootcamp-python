05d4cff26480ab371f3212a6f592cd01d9efebaa
Icaro Pinheiro <icaroalan27@gmail.com>
2024-11-24 17:52:55 -0300

    update files check

--- bootcamp-python-2/stone_etl.py
+++ bootcamp-python-2/stone_etl.py
@@ -7,11 +7,8 @@ import glob
 
 
 caminho: str = r"C:\Users\icaro\OneDrive\ENDOVIDA\CONTABILIDADE\DATABASE\STONE\2023"
-
 arquivos_csv = glob.glob(os.path.join(caminho, '*.csv'))
-
 df_list = [pd.read_csv(arquivo, encoding='ISO-8859-1', sep=';') for arquivo in arquivos_csv]
-
 df_total = pd.concat(df_list, ignore_index=True)
 
 print(df_total)
